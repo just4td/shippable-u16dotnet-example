b3400236c1da1a28206056d7795145f00c7ace56
Kyle Dodson <kyledodson@gmail.com>
2017-12-06 08:47:34 -0800

    Added modern project

---
+++ MyModernProject/MyApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyLibrary;
+using System;
+using System.Diagnostics;
+
+namespace MyApplication
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var processName = Process.GetCurrentProcess().ProcessName;
+                Console.WriteLine($"Usage: {processName} \"{{\\\"some\\\":\\\"json\\\"}}\"");
+            }
+            else
+            {
+                foreach (var json in args)
+                {
+                    try
+                    {
+                        Console.WriteLine(JsonFormatter.Format(json));
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine($"Invalid JSON: {json}");
+                    }
+                }
+            }
+        }
+    }
+}
